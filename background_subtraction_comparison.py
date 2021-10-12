f41a4f049bcb7fd3654c6484663a2f95c0831061
Xintomane Mahange <69038045+xintomane@users.noreply.github.com>
2021-10-12 15:28:53 +0200

    removed code used to test morphological filters

--- background_subtraction_comparison.py
+++ background_subtraction_comparison.py
@@ -7,7 +7,6 @@ import sys
 
 captured_video = cv.VideoCapture("BMC_dataset\Video_007.avi")
 #captured_video = cv.VideoCapture("highway.mp4") #for testing
-kernel = np.ones((4,4),np.uint8)
 #Adapted from https://github.com/AhadCove/Background-Subtractor-Comparisons/blob/master/main.py
 
 
@@ -40,23 +39,6 @@ while(1):
     GMG_fgMask = GMG.apply(frame)
     CNT_fgMask = CNT.apply(frame)
 
-    MOG_Count = np.count_nonzero(MOG_fgMask)
-    kernel_2 = np.ones((MOG_Count,MOG_Count),np.uint8)
-
-
-    opening_MOG = cv.morphologyEx(MOG_fgMask,cv.MORPH_OPEN,kernel)
-    opening_MOG2 = cv.morphologyEx(MOG_fgMask,cv.MORPH_OPEN,kernel)
-    opening_KNN = cv.morphologyEx(MOG_fgMask,cv.MORPH_OPEN,kernel)
-    opening_GSOC = cv.morphologyEx(MOG_fgMask,cv.MORPH_OPEN,kernel)
-    opening_LSBP = cv.morphologyEx(MOG_fgMask,cv.MORPH_OPEN,kernel)
-    opening_GMG = cv.morphologyEx(MOG_fgMask,cv.MORPH_OPEN,kernel)
-    opening_CNT = cv.morphologyEx(MOG_fgMask,cv.MORPH_OPEN,kernel)
-    #clsoing = cv.morphologyEx(MOG_fgMask,cv.MORPH_CLOSE,kernel)
-    #erosion = cv.morphologyEx(MOG_fgMask,cv.MORPH_ERODE,kernel)
-
-
-
-    
 
     cv.putText(GSOC_fgMask, 'GSOC', (100, 40),
                cv.FONT_HERSHEY_SIMPLEX, 1.2 ,(255,255,255),2, cv.LINE_AA)
@@ -81,10 +63,7 @@ while(1):
     cv.imshow('LSBP', LSBP_fgMask)
     cv.imshow('GMG', GMG_fgMask)
     cv.imshow('CNT', CNT_fgMask)
-    #cv.imshow("Opening",opening)
-    #cv.imshow("Closing",clsoing)
-    #cv.imshow('erosion',erosion)
-   # print(MOG_Count)
+
 
 
 
