53b8c68b824051533960d4bd8f79a9ac7e36d170
Xintomane Mahange <69038045+xintomane@users.noreply.github.com>
2021-10-14 16:30:19 +0200

    updated parameters

--- test.py
+++ test.py
@@ -4,6 +4,15 @@ import argparse
 import os
 import glob
 
+kernel = cv.getStructuringElement(cv.MORPH_RECT, (3,3))
+GSOC = cv.bgsegm.createBackgroundSubtractorGSOC()
+KNN = cv.createBackgroundSubtractorKNN(100,400,True)
+MOG2 = cv.createBackgroundSubtractorMOG2(300,400,False)
+MOG = cv.bgsegm.createBackgroundSubtractorMOG(300)
+LSBP = cv.bgsegm.createBackgroundSubtractorLSBP(nSamples=20,LSBPRadius=16,Tlower=2.0,Tupper=32.0,Tinc= 1.0, Tdec= 0.05, Rscale= 10.0, Rincdec=0.005, LSBPthreshold=8)
+GMG = cv.bgsegm.createBackgroundSubtractorGMG(10,.8)
+CNT = cv.bgsegm.createBackgroundSubtractorCNT()
+{'nSamples': 20, 'LSBPRadius': 16, 'Tlower': 2.0, 'Tupper': 32.0, 'Tinc': 1.0, 'Tdec': 0.05, 'Rscale': 10.0, 'Rincdec': 0.005, 'LSBPthreshold': 8}
 def main():
 
     def contains_relevant_files(root):
@@ -30,7 +39,7 @@ def main():
         return gt, f
 
 
-    path = './real\Video_001'
+    path = './real\Video_005'
     path_dir = find_relevant_dirs(path)
     print(len(path_dir))
     for seq in path_dir:
@@ -41,14 +50,31 @@ def main():
 
     bgs = cv.createBackgroundSubtractorMOG2()
     for i in range(len(ground)):
+        
         frame[i] = np.uint8(cv.imread(frame[i], cv.IMREAD_COLOR))
+        frame[i]= cv.morphologyEx(frame[i],cv.MORPH_OPEN,kernel,iterations = 2)
         ground[i] = np.uint8(cv.imread(ground[i], cv.IMREAD_COLOR))
+        
         cv.imshow('Frame', frame[i])
         cv.imshow('Ground-truth', ground[i])
-        mask = bgs.apply(frame[i])
-        bg = bgs.getBackgroundImage()
-        cv.imshow('BG', bg)
-        cv.imshow('Output mask', mask)
+       # mask = bgs.apply(frame[i])
+        GSOC_fgMask = GSOC.apply(frame[i])
+        KNN_fgMask = KNN.apply(frame[i])
+        MOG2_fgMask = MOG2.apply(frame[i])
+        MOG_fgMask = MOG.apply(frame[i])
+        LSBP_fgMask = LSBP.apply(frame[i])
+        GMG_fgMask = GMG.apply(frame[i])
+        CNT_fgMask = CNT.apply(frame[i])
+        #bg = bgs.getBackgroundImage()
+        #cv.imshow('BG', bg)
+        # cv.imshow('Frame', frame)
+        cv.imshow('GSOC', GSOC_fgMask)
+        cv.imshow('KNN', KNN_fgMask)
+        cv.imshow('MOG', MOG_fgMask)
+        cv.imshow('MOG2', MOG2_fgMask)
+        cv.imshow('LSBP', LSBP_fgMask)
+        cv.imshow('GMG', GMG_fgMask)
+        cv.imshow('CNT', CNT_fgMask)
         k = cv.waitKey(0)
         if k == 27:
             break
